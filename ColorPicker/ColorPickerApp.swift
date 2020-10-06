9d7725f0cfeef6f9f3b315940487854d3c99d327
Max <maxim.macari.dam@gmail.com>
2020-10-07 01:21:04 +0200

    Initial Commit

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Maxim Macari on 07/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
